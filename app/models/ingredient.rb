1f709a1210dfe9cd9af9b04874cfa85dde14b725
runandrerun <the.asantiagojr@gmail.com>
2018-07-25 11:12:31 -0400

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,8 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+  # 
+  # def recipe_ingredients_attributes=(ing)
+  #   self.recipe = Recipe.find_or_create_by(name: ing.name)
+  #   self.recipe.update
+  # end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+
+  # def recipe_ingredients_attributes 
+  #
+  # end
 end
